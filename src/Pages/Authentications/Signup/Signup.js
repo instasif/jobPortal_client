6c7bc06a67d34da5152a3936ab7d99ae8ccf7104
instasif <asifh9277@gmial.com>
2023-03-11 03:10:39 +0600

    customized

--- src/Pages/Authentications/Signup/Signup.js
+++ src/Pages/Authentications/Signup/Signup.js
@@ -40,8 +40,7 @@ const Signup = () => {
           <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
 
           <p className="mt-4 text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero
-            nulla eaque error neque ipsa culpa autem, at itaque nostrum!
+            Fill up with your name, email, and password.
           </p>
         </div>
 
